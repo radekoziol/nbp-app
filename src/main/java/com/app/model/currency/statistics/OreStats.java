b2a2033b840f610850a02c8aadf03892409d2edf
radekoziol <radekoziol@wp.pl>
2019-03-13 09:47:52 +0100

    Statistics classes refactor

--- src/main/java/com/app/model/currency/statistics/OreStats.java
+++ src/main/java/com/app/model/currency/statistics/OreStats.java
@@ -25,23 +25,40 @@ public class OreStats extends ListStats{
      * @return
      * @throws IOException
      */
-    public double getAverageGoldPrize(Date startDate, Date endDate) throws IOException, InterruptedException {
+    public double getAverageGoldPrice(Date startDate, Date endDate) throws IOException, InterruptedException {
+
+        Request request = createRequestForAverageGoldPrice(startDate,endDate);
+
+        return calculateAverageGoldPriceFromRequest(request);
+    }
+
+    private double calculateAverageGoldPriceFromRequest(Request request) throws IOException, InterruptedException {
+
+        List<Ore> orePrices = createListOfGoldPricesFromRequest(request);
+
+        return super.getAverageOf(orePrices, Ore::getCena);
+    }
+
+    private List<Ore> createListOfGoldPricesFromRequest(Request request) throws IOException, InterruptedException {
+
+        OreQuery oreQuery = new OreQuery();
+        List<Ore[]> oreTablePrices = oreQuery.getAllObjectsFrom(request);
+        List<Ore> orePrices = new ArrayList<>();
+        oreTablePrices
+                .forEach(t -> orePrices.addAll(Arrays.asList(t)));
+
+        return orePrices;
+    }
+
+    private Request createRequestForAverageGoldPrice(Date startDate, Date endDate) {
 
         Request.RequestBuilder requestBuilder = new Request.RequestBuilder();
-        Request request = requestBuilder
+        return requestBuilder
                 .setCode("cenyzlota/")
                 .setStartDate(startDate)
                 .setReturnType(Ore[].class)
                 .setEndDate(endDate)
                 .build();
-
-        OreQuery oreQuery = new OreQuery();
-        List<Ore[]> oreTablePrizes = oreQuery.getAllObjectsFrom(request);
-        List<Ore> orePrizes = new ArrayList<>();
-        oreTablePrizes
-                .forEach(t -> orePrizes.addAll(Arrays.asList(t)));
-
-        return super.getAverageOf(orePrizes, Ore::getCena);
     }
 
 
@@ -50,19 +67,29 @@ public class OreStats extends ListStats{
      * @param date
      * @return
      */
-    public double getGoldPrize(Date date) throws InvalidArgumentException, IOException, InterruptedException {
+    public double getGoldPrice(Date date) throws IOException, InterruptedException {
+
+        Request request = createRequestForGoldPrice(date);
+
+        return getGoldPriceFromRequest(request);
+    }
+
+    private double getGoldPriceFromRequest(Request request) throws IOException, InterruptedException {
 
         OreQuery oreQuery = new OreQuery();
+        Ore[] ores = oreQuery.getObjectFrom(request);
+
+        return ores[0].getCena();
+    }
+
+    private Request createRequestForGoldPrice(Date date) {
 
         Request.RequestBuilder requestBuilder = new Request.RequestBuilder();
-        Request request = requestBuilder
+        return requestBuilder
                 .setCode("cenyzlota/")
                 .setReturnType(Ore[].class)
                 .setStartDate(date)
                 .build();
-
-        Ore[] ores = oreQuery.getObjectFrom(request);
-        return ores[0].getCena();
     }
 
 }
